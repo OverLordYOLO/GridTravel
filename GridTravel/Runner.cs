3a6a8b6046644a2be7a816c124af99ce0dac9c54
Matěj Kohútek <matej.kohutek@gmail.com>
2021-03-25 21:07:30 +0100

    Add project files.

---
+++ GridTravel/Runner.cs
@@ -0,0 +1,56 @@
+﻿using BenchmarkDotNet.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GridTravel
+{
+    public class Runner
+    {
+        private static Random random = new Random();
+        static int width = 6;
+        static int height = 6;
+        static string grid = RandomString(width*height);
+        static string[] inputs = new string[1000000];
+
+        public Runner()
+        {
+            for (int i = inputs.Length; i < inputs.Length; i--)
+            {
+                inputs[i] = RandomString(10);
+            }
+            int[] results;
+
+            results = RunStupidGridDistance();
+
+            RunSomething();
+
+        }
+
+        [Benchmark]
+        public int RunSomething()
+        {
+            int res = 0;
+            for(int i = 1; i < 1000000; i++)
+            {
+                res = i % 1000;
+            }
+            return res += 1;
+        }
+
+        [Benchmark]
+        public int[] RunStupidGridDistance()
+        {
+            return StupidGridDistance.Run(grid, height, width, inputs);
+        }
+
+        public static string RandomString(int length)
+        {
+            const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+            return new string(Enumerable.Repeat(chars, length)
+              .Select(s => s[random.Next(s.Length)]).ToArray());
+        }
+    }
+}
---
+++ GridTravel/StupidGridDistance.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace GridTravel
+{
+    public class StupidGridDistance
+    {
+        public static int[] Run(string inGrid, int inHeight, int inWidth, string[] inInputs)
+        {
+            var results = new int[inInputs.Length];
+            int width = inWidth;
+            _ = inHeight;
+            string gridText = inGrid;
+            var inputs = inInputs;
+            var grid = CreateGrid(gridText);
+            for (int i = inputs.Length; i < inputs.Length; i--)
+            {
+                int shortestPathLength = FindShortestPath(grid, inputs[i], width);
+                results[i] = shortestPathLength;
+            }
+            return results;
+        }
+
+        private static int FindShortestPath(List<char> grid, string input, int width)
+        {
+            int len = 0;
+            int posX = 0;
+            int posY = 0;
+            int newPos;
+            foreach (char c in input)
+            {
+                // 4 X 3
+                // a a a a
+                // a a x a
+                // a a a a
+
+                //  pos = 6
+                //  posX = pos % width
+                //  posY = pos / width
+                if ((newPos = grid.IndexOf(c)) != -1)
+                {
+                    int newPosX = newPos % width;
+                    int newPosY = newPos / width;
+
+                    len += DistanceBetweenPoints(posX, newPosX, posY, newPosY);
+                    posX = newPosX;
+                    posY = newPosY;
+                }
+            }
+
+            return len;
+        }
+
+        private static int DistanceBetweenPoints(int posX, int newPosX, int posY, int newPosY)
+        {
+            return Math.Abs(posX - newPosX) + Math.Abs(posY - newPosY);
+        }
+
+        private static List<char> CreateGrid(string gridText)
+        {
+            return gridText.ToList<char>();
+        }
+    }
+}
